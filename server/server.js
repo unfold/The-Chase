4c8c20abeeed99a92b8b992b3f1dd7b6bd3411ec
Daniel <daniel@unfold.no>
2011-02-24 00:08:27 +0100

    Hooked up controller with the server. working like a charm

--- server/server.js
+++ server/server.js
@@ -1,33 +1,22 @@
 var fs = require('fs')
 var http = require('http')
 var io = require('../lib/socket.io')
-// var User = require('../model/user.js')
 
-var server = http.createServer(function(request, response) {
-	response.writeHead(400);
-	response.write('404');
-	response.end();
-})
+var server = http.createServer();
 
 server.listen(8080);
 
 var socket = io.listen(server);
-socket.on('connection', function(client) {
-	console.log(client.sessionId + ' connected.');
-	client.broadcast(JSON.stringify({type: 'welcome', user: { id: client.sessionId}}));
 
-	client.on('message', function(message) {
-		var data = JSON.parse(message);
-		
-		switch (data.type) {
-			case 'update':
-				client.broadcast(JSON.stringify({type: 'update', user: { id: client.sessionId, angle: data.angle }}));
-			break;
-		}
+socket.on('connection', function(client) {
+	client.broadcast({type: 'connect', userId: client.sessionId });
+	
+	client.on('message', function(data) {
+		data.userId = client.sessionId;
+		client.broadcast(data);
 	});
 	
 	client.on('disconnect', function(message) {
-		console.log(client.sessionId + ' disconnected.');
-		client.broadcast(JSON.stringify({type: 'disconnect', user: { id: client.sessionId }}));
+		client.broadcast({type: 'disconnect', userId: client.sessionId });
 	});
 });
